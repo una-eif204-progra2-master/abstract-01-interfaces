cc59d37abfc9bc005855fe185223b6c42c01d106
Maikol Guzman Alan <mguzmana@akamai.com>
2020-08-18 00:34:15 -0600

    Minor change
    Signed-off-by: Maikol Guzman Alan <mguzmana@akamai.com>

--- Main.cpp
+++ Main.cpp
@@ -2,9 +2,12 @@
 // Created by Maikol Guzman on 8/17/20.
 //
 
+#include <iostream>
 #include "TapePlayer.h"
 
 int main() {
+    std::cout << "Welcome to the UNA! (VIRTUAL)" << std::endl;
+
     TapePlayer tapePlayer;
 
     tapePlayer.play();
@@ -13,7 +16,9 @@ int main() {
     tapePlayer.reverse();
     tapePlayer.record();
 
-    // using pointers it's possible to associate the implementation.
+    // IPlayer iPlayer; // Error because it's an abstract class
+
+    // using pointers it's possible to associate the implementation, because the Pointer not create the instance
     IPlayer *player;
     player = &tapePlayer;
     player->play();
